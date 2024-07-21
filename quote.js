07b83a1749a59f6f8ad53a2ddb5e59a13276f014
unknown <rukayattijani2@gmail.com>
2024-07-21 18:24:56 +1000

    centered discussion and made fonts for paragraph and header, and the title is stylized now. almost done

--- quote.js
+++ quote.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function()
 {
     const quotes = document.querySelectorAll('.quote');
+    let CurrentSound = null;
 
     quotes.forEach(quote => 
     {
         quote.addEventListener('click', function()
          {
-           new Audio(quote.getAttribute('sound')).play();
+          if(CurrentSound)
+          {
+            CurrentSound.pause();
+          }
+          CurrentSound = new Audio(quote.getAttribute('sound'))
+          CurrentSound.play();
          })
          ;
     });
