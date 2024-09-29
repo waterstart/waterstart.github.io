587872418a3f41f01e50cd2792254f5e28c0c375
Abdulrahmon tijani <schoolrumblefan2008@gmail.com>
2024-09-29 20:10:07 +1000

    added some test functionality, taking a break from this one, will start on linear algebra and stuff

--- Scripts/Test.js
+++ Scripts/Test.js
@@ -1,11 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-
-const testthing = document.getElementById('myElement');
-const fulldate = new Date().toDateString();
-
-console.log(fulldate);
-
-testthing.innerText=fulldate;
-
-});
+(function () {
+    // detect touch
+    if ("ontouchstart" in document.documentElement) {
+      document.documentElement.className += " touch-device";
+    }
+  
+    const scroller = document.querySelector(".navwrap");
+    const dropDown = document.querySelectorAll(".dropdown");
+    scroller.addEventListener("scroll", checkScroll);
+  
+    function checkScroll() {
+      document.activeElement.blur();
+     scroller.classList.add("isScrolling");
+      for (let i = 0; i < dropDown.length; i++) {
+        dropDown[i].style.transform =
+          "translateX(-" + scroller.scrollLeft + "px)";
+      }
+     scroller.classList.remove("isScrolling");
+    }
+  })();
+  
+  /* 
+  SEE Sitepoint Thread: 
+  https://www.sitepoint.com/community/t/css-drop-down-menu-hidden-behind-horizontal-scrollbar/367783/16 
+  */
+  
